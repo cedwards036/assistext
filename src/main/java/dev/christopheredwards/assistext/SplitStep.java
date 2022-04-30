781b57595c9b5fd08232eefd875942fe173dd966
Christopher Edwards <cedwards036@gmail.com>
2022-04-30 10:50:21 -0400

    implement SplitStep

---
+++ src/main/java/dev/christopheredwards/assistext/SplitStep.java
@@ -0,0 +1,72 @@
+package dev.christopheredwards.assistext;
+
+import java.util.regex.Pattern;
+
+/**
+ * A text transformation pipeline step that splits the given string into multiple lines.
+ * By default, text will be split on the comma character (","). This split string can be
+ * specified as either a literal string or as a regular expression.
+ */
+public class SplitStep implements PipelineStep {
+
+    private String rawSplitString;
+    private String lineEnding;
+    private boolean useRegex;
+
+    public SplitStep() {
+        setSplitString(",");
+        setUseWindowsLineEnding(false);
+        setUseRegex(false);
+    }
+
+    /**
+     * Set whether the transformation will output a string with Windows-style line endings or not.
+     *
+     * @param useWindowsLineEnding if true, transformation result will have Windows-style (\r\n) line endings; Linux-style (\n) otherwise
+     */
+    public void setUseWindowsLineEnding(boolean useWindowsLineEnding) {
+        if (useWindowsLineEnding) {
+            this.lineEnding = "\r\n";
+        } else {
+            this.lineEnding = "\n";
+        }
+    }
+
+    /**
+     * Specify the string on which the step input will be split.
+     *
+     * @param splitString a string to replace with an appropriate newline character in the step input
+     */
+    public void setSplitString(String splitString) {
+        this.rawSplitString = splitString;
+    }
+
+    /**
+     * Set whether the split string will be interpreted as a regular expression or not.
+     *
+     * @param useRegex if true, output will be split on any string matching the regex split string; otherwise, split string will be interpreted as plain text
+     */
+    public void setUseRegex(boolean useRegex) {
+        this.useRegex = useRegex;
+    }
+
+    /**
+     * Produce a copy of the given string that is split on a pre-specified substring.
+     *
+     * @param s the string being split
+     * @return a split copy of the string
+     */
+    @Override
+    public String transform(String s) {
+        if (s == null) {
+            throw new InvalidInputException("null");
+        }
+        String splitString;
+        if (useRegex) {
+            splitString = rawSplitString;
+        } else {
+            splitString = Pattern.quote(rawSplitString);
+        }
+        return String.join(lineEnding, s.split(splitString));
+    }
+}
---
+++ src/test/java/dev/christopheredwards/assistext/SplitStepTest.java
@@ -0,0 +1,61 @@
+package dev.christopheredwards.assistext;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class SplitStepTest {
+
+    @Test
+    void test_null() {
+        SplitStep splitStep = new SplitStep();
+        assertThrows(PipelineStep.InvalidInputException.class, () -> splitStep.transform(null));
+    }
+
+    @Test
+    void does_nothing_to_empty_string() {
+        SplitStep splitStep = new SplitStep();
+        assertEquals("", splitStep.transform(""));
+    }
+
+    @Test
+    void splits_on_comma_by_default() {
+        SplitStep splitStep = new SplitStep();
+        String input = "a,b,c";
+        assertEquals("a\nb\nc", splitStep.transform(input));
+    }
+
+    @Test
+    void splits_on_user_specified_string() {
+        SplitStep splitStep = new SplitStep();
+        splitStep.setSplitString("SP");
+        String input = "aSPbSPc";
+        assertEquals("a\nb\nc", splitStep.transform(input));
+    }
+
+    @Test
+    void outputs_windows_line_endings_if_specified() {
+        SplitStep splitStep = new SplitStep();
+        splitStep.setUseWindowsLineEnding(true);
+        String input = "a,b,c";
+        assertEquals("a\r\nb\r\nc", splitStep.transform(input));
+    }
+
+    @Test
+    void windows_line_ending_setting_does_not_override_any_existing_line_ending_characters_in_input() {
+        SplitStep splitStep = new SplitStep();
+        splitStep.setUseWindowsLineEnding(true);
+        String input = "a\n1,b\n2,c\n3";
+        assertEquals("a\n1\r\nb\n2\r\nc\n3", splitStep.transform(input));
+    }
+
+    @Test
+    void split_string_is_interpreted_as_regex_if_option_is_set() {
+        SplitStep splitStep = new SplitStep();
+        splitStep.setUseRegex(true);
+        splitStep.setSplitString("[,;]");
+        String input = "a,b;c";
+        assertEquals("a\nb\nc", splitStep.transform(input));
+    }
+}
